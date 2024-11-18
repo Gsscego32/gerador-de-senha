f29c58fb119b372ca44a300a2c305def38b60f99
Gsscego32 <111784522+Gsscego32@users.noreply.github.com>
2024-11-18 11:44:43 -0300

    Add files via upload

--- gerador de senha/main.js
+++ gerador de senha/main.js
@@ -1,5 +1,19 @@
 const numeroSenha =document.querySelector('.parametro-senha-texto');
 let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
-const botoes =document.querySelector('parametro-senha-botoes');
- botoes[0].onclick =diminuirTamanho;
+const botoes =document.querySelectorAll('.parametro-senha-botao');
+ botoes[0].onclick =diminuirTamanho; 
+ function diminuirTamanho( ){
+if(tamanhoSenha>6){
+ tamanhoSenha= tamanhoSenha-1
+numeroSenha.textContent=tamanhoSenha
+}
+ }
+
+ botoes[1].onclick =aumentarTamanho;
+ function aumentarTamanho( ){
+    if(tamanhoSenha<15){
+     tamanhoSenha=tamanhoSenha+1;
+    numeroSenha.textContent=tamanhoSenha;
+    }
+ }
